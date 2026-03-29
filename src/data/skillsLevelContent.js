1d80c07f743b543ea83fcc131448bfc5647d0085
AbdallahBenj <abdallahbenjalal@gmail.com>
2026-03-30 00:04:29 +0100

    update level skils

--- src/data/skillsLevelContent.js
+++ src/data/skillsLevelContent.js
@@ -1,9 +1,9 @@
 const skillsLevelContent = [
-  { id: 1, name: "React", level: "70%" },
-  { id: 2, name: "Tailwind", level: "75%" },
-  { id: 3, name: "JavaScript (ES6+)", level: "80%" },
-  { id: 4, name: "HTML5", level: "96%" },
-  { id: 5, name: "CSS3", level: "90%" },
+  { id: 1, name: "HTML5", level: "85%" },
+  { id: 2, name: "CSS3", level: "75%" },
+  { id: 3, name: "Tailwind", level: "70%" },
+  { id: 4, name: "JavaScript (ES6+)", level: "65%" },
+  { id: 5, name: "React", level: "60%" },
 ];
 
 export default skillsLevelContent;
--- src/data/toolsContent.js
+++ src/data/toolsContent.js
@@ -4,10 +4,12 @@ const toolsContent = [
   { id: 3, name: "vite", color: "bg-emerald-500" },
   { id: 4, name: "npm", color: "bg-pink-500" },
   { id: 5, name: "ui/ux", color: "bg-indigo-500" },
-  { id: 6, name: "Responsive", color: "bg-sky-500" },
-  { id: 7, name: "Design", color: "bg-blue-500" },
-  { id: 8, name: "Accessibility", color: "bg-purple-500" },
-  { id: 9, name: "Performance", color: "bg-amber-500" },
+  { id: 6, name: "Headless UI", color: "bg-green-500" },
+  { id: 7, name: "shadcn/ui", color: "bg-orange-500" },
+  { id: 8, name: "Responsive", color: "bg-sky-500" },
+  { id: 9, name: "Design", color: "bg-blue-500" },
+  { id: 10, name: "Accessibility", color: "bg-purple-500" },
+  { id: 11, name: "Performance", color: "bg-amber-500" },
 ];
 
 export default toolsContent;
